667cc04d865434c27053e5b84e7b09442fcfca5f
Anjal Afghan <anjal.afghan93@gmail.com>
2018-10-27 09:06:52 +0530

    Changed it according to the server -- Change dbhost

--- initials.php
+++ initials.php
@@ -1,6 +1,6 @@
 <?php
 $dbhost="localhost";
-$dbuser="anjal";
-$dbpass="anjal";
-$dbname="student_feedback_survey";
+$dbuser="skharghar_anjal";
+$dbpass="Anjal@12345";
+$dbname="skharghar_student_feedback_survey";
 ?>
