57d74ce404a2f01258f1994649c0869caaf5982e
Anjal Afghan <anjal.afghan93@gmail.com>
2018-10-30 12:38:48 +0530

    added changes to structure and database

--- add.php
+++ add.php
@@ -17,6 +17,7 @@ $connection= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+      <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
 
       <!--Let browser know website is optimized for mobile-->
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
@@ -46,6 +47,30 @@ $connection= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
         </div>
       </div>
       <div class="row">
+     <div class="input-field col s12">
+       <input id="rollno" name="rollno" type="text" class="validate" required>
+       <label for="rollno">Roll No</label>
+     </div>
+   </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="firstname" name="firstname" type="text" class="validate" required>
+          <label for="firstname">First Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="middlename" name="middlename" type="text" class="validate" required>
+          <label for="middlename">Middle Name</label>
+        </div>
+      </div>
+      <div class="row">
+        <div class="input-field col s12">
+          <input id="lastname" name="lastname" type="text" class="validate" required>
+          <label for="lastname">Last Name</label>
+        </div>
+      </div>
+      <div class="row">
         <div class="input-field col s12">
           <input id="username" name="username" type="text" class="validate" required>
           <label for="username">User Name</label>
@@ -57,12 +82,18 @@ $connection= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
           <label for="password">Password</label>
         </div>
       </div>
-      <div class="row">
-        <div class="input-field col s12">
-          <input id="department" name="department" type="text" class="validate" required>
-          <label for="department">Department</label>
-        </div>
-      </div>
+
+      <div class="input-field col s12">
+    <select name="department">
+      <option value="" disabled selected></option>
+      <option value="INFORMATION TECHNOLOGY">INFORMATION TECHNOLOGY</option>
+      <option value="CIVIL">CIVIL</option>
+      <option value="COMPUTER SCIENCE">COMPUTER SCIENCE</option>
+      <option value="MECHANICAL">MECHANICAL</option>
+      <option value="ELECTRONICS AND TELECOMMUNICATION">EXTC</option>
+    </select>
+    <label>Select your Department</label>
+  </div>
       <div class="row">
         <div class="input-field col s12">
           <input id="division" name="division" type="text" class="validate">
@@ -80,12 +111,18 @@ $connection= new mysqli($dbhost,$dbuser,$dbpass,$dbname);
       </div>
     </div>
   </div>
-      <script type="text/javascript" src="js/materialize.min.js"></script>
+      <script type="text/javascript" src="js/materialize.min.js">
+
+      </script>
       <script>
 function submitForm(){
   document.getElementById('form').submit();
 
 }
+
+$(document).ready(function(){
+  $('select').formSelect();
+});
 </script>
 <style>
 .cont{
--- addstudent.php
+++ addstudent.php
@@ -3,16 +3,19 @@ session_start();
 include_once 'initials.php';
 
 $connection= mysqli_connect($dbhost,$dbuser,$dbpass,$dbname);
-$grno = $_POST['grno'];
-$username = $_POST['username'];
-$password = $_POST['password'];
+$grno       = $_POST['grno'];
+$rollno     = $_POST['rollno'];
+$firstname  = $_POST['firstname'];
+$middlename = $_POST['middlename'];
+$lastname   = $_POST['lastname'];
+$username   = $_POST['username'];
+$password   = $_POST['password'];
 $department = $_POST['department'];
-$division = $_POST['division'];
+$division   = $_POST['division'];
 
-mysqli_query($connection,"INSERT INTO student (gr_no,username,password,department,division) VALUES ('$grno','$username','$password','$department','$division')");
-
+ mysqli_query($connection,"INSERT INTO student (gr_no,roll_no,first_name,middle_name,last_name,username,password,department,division) VALUES ('$grno',$rollno,'$firstname','$middlename','$lastname','$username','$password','$department','$division')");
 echo "Student added Successfully" ;
-
-header('Refresh:1;URL=add.php');
+echo "<script type='text/JavaScript'> window.location='add.php';</script>";
+exit();
 
  ?>
